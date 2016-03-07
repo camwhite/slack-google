d9528cbf24a292c454d32886d34561fbb34cc5c1
Cameron White <camw85@mac.com>
2016-03-07 13:27:07 +0700

    :nut_and_bolt: convert to slash command

--- app.js
+++ app.js
@@ -1,4 +1,37 @@
+'use strict';
+
+const express = require('express');
 const config = require('./config');
 const Bot = require('./utils/Bot');
 
-var bot = new Bot(config);
+let app = express();
+let bot = new Bot();
+
+app.get('/', (req, res) => {
+  if(req.query.token != config.token) return res.sendStatus(401);
+
+  bot.searchGoogle(req.query.text).then((results) => {
+    let message = {
+      response_type: 'in_channel',
+      attachments: []
+    };
+
+    for(var result of results) {
+      let attachment = {
+        title: result.title,
+        title_link: result.href,
+        text: result.description
+      };
+      if(!/^Images/.test(result.title)) {
+        message.attachments.push(attachment);
+      }
+    }
+
+    res.json(message);
+  })
+  .catch((err) => {
+    res.sendStatus(err);
+  });
+});
+
+app.listen(9000);
--- utils/Bot.js
+++ utils/Bot.js
@@ -1,42 +1,20 @@
 'use strict';
 
-const chalk = require('chalk');
 const google = require('google');
-const Slack = require('slack-client');
-
 
 class Bot {
-  constructor(opts) {
-    this.opts = opts;
+  constructor() {
     this.google = google;
-
-    this.slack = new Slack(opts.token);
-    this.slack.login();
-
-    let pattern = /^google\?/;
-    this.slack.on('message', (message) => {
-      if(pattern.test(message.text)) {
-        let searchTerm = message.text.split(' ').slice(1, message.text.length).join(' ');
-        this.searchGoogle(searchTerm);
-      }
-    })
   }
   searchGoogle(query) {
-    this.google(query, (err, next, links) => {
-      const channel = this.slack.getChannelGroupOrDMByName(this.opts.channel);
-      let topFiveLinks = links.slice(0, 4);
-      for(var link of topFiveLinks) {
-        let message = {
-          text: `${link.href}\n${link.description}`,
-          username: this.opts.nickname,
-          icon_url: 'http://www.theweeklyreporter.com/wp-content/uploads/2015/09/G_is_For_Google_New_Logo_Thumb.png',
-          parse: 'full',
-        }
-        channel.postMessage(message);
-      }
-
-
+    let promise = new Promise((resolve, reject) => {
+      this.google(query, (err, next, links) => {
+        if(err) return reject(err);
+        let topFiveLinks = links.slice(0, 6);
+        resolve(topFiveLinks);
+      });
     });
+    return promise;
   }
 }
 
